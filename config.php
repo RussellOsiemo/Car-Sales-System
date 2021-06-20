933e6c7cdc3c6c2a8e697b053ced0a208a38eb7b
RussellOsiemo <russelosiemo@gmail.com>
2021-06-19 18:27:54 -0700

    changed config.php

--- config.php
+++ config.php
@@ -13,8 +13,4 @@ $con= mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD,$DB_NAME);
 if($con === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-//elseif ($con==true) {
-	//echo "connection successful";
-	# code...
-//}
 ?>

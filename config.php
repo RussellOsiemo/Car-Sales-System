e0f25692b01325b7fde3be0d77af3e503719c1bf
RussellOsiemo <russelosiemo@gmail.com>
2021-06-19 18:18:42 -0700

    first commit

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$DB_SERVER="localhost";
+$DB_USERNAME="root";
+$DB_PASSWORD="";
+$DB_NAME="Car Sales";
+ 
+/* Attempt to connect to MySQL database */
+$con= mysqli_connect($DB_SERVER, $DB_USERNAME, $DB_PASSWORD,$DB_NAME);
+ 
+// Check connection
+if($con === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+//elseif ($con==true) {
+	//echo "connection successful";
+	# code...
+//}
+?>
